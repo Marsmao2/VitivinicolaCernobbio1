10c5bbb9dde7f0d6feecb9cc6f5e46803c838add
Marsmao2 <maumarson@gmail.com>
2025-06-03 22:07:27 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,25 +3,24 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="bg-olive-700 text-white fixed w-full z-20">
-      <nav className="max-w-6xl mx-auto px-4 py-4 flex justify-between items-center">
+      <nav className="max-w-6xl mx-auto px-4 py-2 flex justify-between items-center">
         {/* Logo / Nome attività */}
         <div className="text-xl font-bold">
           Vitivinicola Cernobbio
         </div>
 
         {/* Link di navigazione */}
-        <div className="space-x-8 flex">
-          <a href="#esperienze" className="cursor-pointer hover:text-gray-200">
+        <div className="flex space-x-8">
+          <a href="#esperienze" className="hover:text-gray-200">
             Experiences
           </a>
-          <a href="#contatti" className="cursor-pointer hover:text-gray-200">
+          <a href="#contatti" className="hover:text-gray-200">
             Contacts
           </a>
-          <a href="#gallery" className="cursor-pointer hover:text-gray-200">
+          <a href="#gallery" className="hover:text-gray-200">
             Gallery
           </a>
         </div>
       </nav>
     </header>
-  );
 }
