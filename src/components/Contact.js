720ab9827b80184a907fe91a1e4feeb8873d9f4e
Marsmao2 <maumarson@gmail.com>
2025-06-03 22:33:30 +0200

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,19 +8,42 @@ export default function Contact() {
         <h2 className="text-3xl font-semibold mb-6 text-center">Contatti & Prenotazioni</h2>
         <div className="space-y-4 text-lg">
           <p className="flex items-center">
-            <FaMapMarkerAlt className="mr-2 text-gold" /> Indirizzo: Via Ronco 33, 22026 Maslianico (CO), Italia
+            <FaMapMarkerAlt className="mr-2 text-gold" />
+            <a
+              href="https://www.google.it/maps/place/Gigi+e+Cristina+Winery+Como+Lake+,+vino+lago+di+Como,+Vitivinicola+Cernobbio,+Gold+medal+Heroic+Wineyard,+Viticoltura+eroica/@45.8480911,9.046156,17z/data=!3m1!4b1!4m6!3m5!1s0x47869d7d9a9694fb:0x581008da0cfda24d!8m2!3d45.8480911!4d9.0487309!16s%2Fg%2F11pqcq5jzv?hl=it&entry=ttu&g_ep=EgoyMDI1MDUyOC4wIKXMDSoASAFQAw%3D%3D"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-olive-800 hover:underline"
+            >
+              Via Ronco 33, 22026 Maslianico (CO), Italia
+            </a>
           </p>
           <p className="flex items-center">
             <FaPhoneAlt className="mr-2 text-gold" /> Telefono:
-            <a href="tel:+393338746401" className="ml-1 text-olive-800 hover:underline">+39 333 8746401</a>
+            <a
+              href="tel:+393338746401"
+              className="ml-1 text-olive-800 hover:underline"
+            >
+              +39 333 8746401
+            </a>
           </p>
           <p className="flex items-center">
             <FaEnvelope className="mr-2 text-gold" /> Email:
-            <a href="mailto:vitivinicolacernobbio@gmail.com" className="ml-1 text-olive-800 hover:underline">vitivinicolacernobbio@gmail.com</a>
+            <a
+              href="mailto:vitivinicolacernobbio@gmail.com"
+              className="ml-1 text-olive-800 hover:underline"
+            >
+              vitivinicolacernobbio@gmail.com
+            </a>
           </p>
           <p className="flex items-center">
             <FaWhatsapp className="mr-2 text-gold" /> Prenota su WhatsApp:
-            <a href="https://wa.me/393338746401" className="ml-1 text-olive-800 hover:underline">Chatta ora</a>
+            <a
+              href="https://wa.me/393338746401"
+              className="ml-1 text-olive-800 hover:underline"
+            >
+              Chatta ora
+            </a>
           </p>
           <div className="mt-4 flex space-x-6">
             <a
