6fa98ea4daeed86d10030dd85f6dfff058419fea
Marsmao2 <maumarson@gmail.com>
2025-06-03 22:12:56 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,26 +2,32 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className="bg-olive-700 text-white fixed w-full z-20">
-      <nav className="max-w-6xl mx-auto px-4 py-2 flex justify-between items-center">
-        {/* Logo / Nome attività */}
-        <div className="text-xl font-bold">
-          Vitivinicola Cernobbio
-        </div>
+    <header>
+      {/* Sottile barra verde sopra tutto */}
+      <div className="bg-olive-900 h-1 w-full" />
 
-        {/* Link di navigazione */}
-        <div className="flex space-x-8">
-          <a href="#esperienze" className="hover:text-gray-200">
-            Experiences
-          </a>
-          <a href="#contatti" className="hover:text-gray-200">
-            Contacts
-          </a>
-          <a href="#gallery" className="hover:text-gray-200">
-            Gallery
-          </a>
-        </div>
-      </nav>
+      {/* Header principale */}
+      <div className="bg-olive-700 text-white fixed w-full z-20">
+        <nav className="max-w-6xl mx-auto px-4 py-2 flex justify-between items-center">
+          {/* Logo / Nome attività */}
+          <div className="text-xl font-bold">
+            Vitivinicola Cernobbio
+          </div>
+
+          {/* Link di navigazione */}
+          <div className="flex space-x-8">
+            <a href="#esperienze" className="hover:text-gray-200">
+              Experiences
+            </a>
+            <a href="#contatti" className="hover:text-gray-200">
+              Contacts
+            </a>
+            <a href="#gallery" className="hover:text-gray-200">
+              Gallery
+            </a>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 }
